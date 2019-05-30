12b5b9a500fd52b175daead7aa66de519cdfd638
TheDG <dsinay@uc.cl>
2019-05-30 01:10:47 -0400

    basic creation

--- app/controllers/ski_routes_controller.rb
+++ app/controllers/ski_routes_controller.rb
@@ -4,5 +4,24 @@
 class SkiRoutesController < ApplicationController
   load_and_authorize_resource
 
-  def get; end
+  def show; end
+
+  def new; end
+
+  def create
+    if @ski_route.save
+      flash[:notice] = 'Ruta creada satisfactoriamente'
+      redirect_to ski_route_path(@ski_route)
+    else
+      flash.now[:alert] = 'Hubo un problema al tratar de crear la ruta'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def ski_route_params
+    params.require(:ski_route).permit(:title, :intro, :distance, :vert,
+                                      :time, :aspect, :dificulty, :route_description,
+                                      :location, :sector)
+          .merge(user: current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'home#index'
-  resources :ski_routes, only: %i[show] do
+  resources :ski_routes, only: %i[show new create edit update] do
     resources :comments, only: %i[create], shallow: true
   end
   resources :users, only: %i[edit update]
